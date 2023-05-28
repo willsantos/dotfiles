b149412b30cd85c267c1b7d13eb9457a27cfe425
Wilson Santos <contato@wilsonsantos.net>
2023-05-28 19:53:11 -0300

    Muda de winget para Scoop

--- windows/programs/dev.ps1
+++ windows/programs/dev.ps1
@@ -1,6 +1,16 @@
 # DEV programs
+# Add scoop bucket
+scoop bucket add extras
+
+scoop install extras/vscode
+scoop install extras/insomnia
+scoop install extras/postman
+scoop install extras/jetbrains-toolbox
+scoop install extras/dbeaver
+scoop install main/nvm
+scoop install extras/rider
+scoop install extras/datagrip
+
 winget install Microsoft.VisualStudio.2022.Professional --silent --accept-package-agreements
-winget install Microsoft.VisualStudioCode --silent --accept-package-agreements
+
 winget install JanDeDobbeleer.OhMyPosh -s winget --silent --accept-package-agreements
-winget install Insomnia.Insomnia --silent --accept-package-agreements
-winget install JetBrains.Toolbox --silent --accept-package-agreements
