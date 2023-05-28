26f081d1489fa6d33d01f0fa24bc0975bd108962
Wilson Santos <contato@wilsonsantos.net>
2023-05-28 16:43:30 -0300

    add basic setup

---
+++ windows/install.ps1
@@ -0,0 +1,12 @@
+# Author: Wilson Santos
+# configuration of the system
+# 1 - Update PowerShell
+winget install Microsoft.Powershell --silent --accept-package-agreements accept-source-agreements
+# 2 - Install Scoop.sh
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+irm get.scoop.sh | iex
+# 3 - Install WSL
+winget install -e --id Canonical.Ubuntu.2204 --silent
+
+
+
