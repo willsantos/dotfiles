49c77c4bc66a16d166a5a1a639173693ca2bf30c
Wilson Santos <contato@wilsonsantos.net>
2023-05-28 20:59:18 -0300

    Add Utils script

---
+++ windows/programs/utils.ps1
@@ -0,0 +1,19 @@
+# Utils
+
+# Add scoop bucket
+scoop bucket add extras
+scoop bucket add nonportable
+scoop bucket add sysinternals
+
+# Install apps
+scoop install extras/flow-launcher
+
+scoop install extras/cpu-z
+scoop install extras/ditto
+scoop install versions/espanso-pre
+scoop install extras/flow-launcher
+scoop install nonportable/everything-np
+scoop install extras/hwmonitor
+scoop install extras/rufus
+scoop install extras/sublime-text
+scoop install sysinternals/zoomit
