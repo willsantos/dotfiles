0297144c93c6c22d84f81887e8bc44544ecfe01f
Wilson Santos <contato@wilsonsantos.net>
2023-05-28 18:35:12 -0300

    fix the path

--- windows/programs/programs.ps1
+++ windows/programs/programs.ps1
@@ -1,8 +1,8 @@
 # Caso não deseje instalar alguma categoria, basta comentar a linha da mesma.
 # Exemplo pc de trabalho não precisa de jogos, então basta comentar a linha & .\games.ps1
-& .\office.ps1
-& .\browsers.ps1
-& .\media.ps1
-& .\peripherals.ps1
-& .\dev.ps1
-& .\games.ps1
+& .\programs\office.ps1
+& .\programs\browsers.ps1
+& .\programs\media.ps1
+& .\programs\peripherals.ps1
+& .\programs\dev.ps1
+& .\programs\games.ps1
