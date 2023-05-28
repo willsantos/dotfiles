6fdaff5ec42c90ca0c260da07649dda6d6ba50eb
Wilson Santos <contato@wilsonsantos.net>
2023-05-28 17:13:29 -0300

    add winget installs por categoria

--- windows/install.ps1
+++ windows/install.ps1
@@ -7,6 +7,7 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 # 3 - Install WSL
 winget install -e --id Canonical.Ubuntu.2204 --silent
-
+# 4 - Instalar os programas por categorias
+& .\programs\programs.ps1
 
 
--- windows/programs/programs.ps1
+++ windows/programs/programs.ps1
@@ -1,3 +1,5 @@
+# Caso não deseje instalar alguma categoria, basta comentar a linha da mesma.
+# Exemplo pc de trabalho não precisa de jogos, então basta comentar a linha & .\games.ps1
 & .\office.ps1
 & .\browsers.ps1
 & .\media.ps1
