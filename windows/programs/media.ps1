e2ec1abfcfe92ddfc70b31f7bfa99000311f3864
Wilson Santos <contato@wilsonsantos.net>
2023-10-31 21:23:50 -0300

    Update media.ps1
    tirar o whataspp beta

--- windows/programs/media.ps1
+++ windows/programs/media.ps1
@@ -7,8 +7,8 @@ scoop bucket add versions
 
 # Install apps
 scoop install extras/vlc
-scoop install versions/whatsapp-beta
+scoop install versions/whatsapp
 scoop install extras/telegram
-scoop install extras/discord
 scoop install extras/spotify
 scoop install extras/zoom
+scoop install extras/discord
