a0f04b388e48001377b38166bd6c907f0b7b6cb5
Wilson Santos <contato@wilsonsantos.net>
2023-10-31 21:22:18 -0300

    Update dev.ps1
    Alterações nos pacotes dev

--- windows/programs/dev.ps1
+++ windows/programs/dev.ps1
@@ -2,17 +2,16 @@
 # Add scoop bucket
 scoop bucket add extras
 # Install Apps
-scoop install extras/vscode
 scoop install extras/insomnia
 scoop install extras/postman
 scoop install extras/jetbrains-toolbox
 scoop install extras/dbeaver
 scoop install main/nvm
-scoop install extras/rider
-scoop install extras/datagrip
 scoop install extras/gitkraken
 
 winget install Microsoft.VisualStudio.2022.Community --silent --accept-package-agreements
+
+winget install -e --id Microsoft.VisualStudioCode --silent --accept-package-agreements
 
 winget install JanDeDobbeleer.OhMyPosh -s winget --silent --accept-package-agreements
 
