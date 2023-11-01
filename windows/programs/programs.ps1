460a7f1adc7085731822f6c265c6be93a2edb34c
Wilson Santos <contato@wilsonsantos.net>
2023-10-31 21:33:32 -0300

    Update programs.ps1
    Remove o git do scoop pq já vai ser instalado via winget

--- windows/programs/programs.ps1
+++ windows/programs/programs.ps1
@@ -1,7 +1,7 @@
 # Caso não deseje instalar alguma categoria, basta comentar a linha da mesma.
 # Exemplo pc de trabalho não precisa de jogos, então basta comentar a linha & .\games.ps1
 # Install git, necessário para alguns pacotes do scoop
-scoop install main/git
+
 # Install categorias
 & .\programs\peripherals.ps1
 & .\programs\office.ps1
