9fb62f55a146e2e9193bbc41e711ba6e26924cee
Wilson Santos <contato@wilsonsantos.net>
2023-12-10 10:56:16 -0300

    Add Firefox Developer

--- windows/programs/browsers.ps1
+++ windows/programs/browsers.ps1
@@ -5,6 +5,7 @@ scoop bucket add extras
 
 scoop install extras/googlechrome
 scoop install extras/firefox
+scoop install versions/firefox-developer
 scoop install extras/brave
 scoop install extras/opera
 scoop install extras/palemoon
