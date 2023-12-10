9361843af49d61d8fd9d1da91de9f7060debd564
Wilson Santos <contato@wilsonsantos.net>
2023-12-10 15:12:17 -0300

    Add diskinfo

--- windows/programs/utils.ps1
+++ windows/programs/utils.ps1
@@ -13,6 +13,7 @@ scoop install versions/espanso-pre
 scoop install extras/flow-launcher
 scoop install nonportable/everything-np
 scoop install extras/hwmonitor
+scoop install extras/crystaldiskinfo
 scoop install extras/rufus
 scoop install extras/sublime-text
 scoop install sysinternals/zoomit
